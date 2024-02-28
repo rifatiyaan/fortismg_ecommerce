cac6a627a269054b8ec81ef8b7eb1438cac155ed
Rifat <mdbrifat@gmail.com>
2024-02-28 14:02:12 +0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header/page";
+import NoticeTop from "./components/NoticeTop/page";
+import SearchNav from "./components/SeachNav/page";
+import NavDropDown from "./components/NavDropDown/page";
+import Banner from "./components/Banner/page";
+import Motto from "./components/Motto/page";
+import Advertise from "./components/Advertise/page";
+import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +19,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme='light'>
+      <body className={inter.className}>
+        <div className="px-1 md:p-0">
+          <div className="">
+        <NoticeTop/>
+        <Header/>
+        <SearchNav/>
+        <NavDropDown/>
+        <Banner/>
+        <Motto/>
+        </div>
+        <Advertise/>
+       
+        
+       
+        {children}
+        </div>
+        </body>
     </html>
   );
 }
